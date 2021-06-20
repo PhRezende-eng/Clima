1406c6738e8143391e43ee0a8f42dfa3650b6212
PhRezende-eng <ph.hr.001@gmail.com>
2021-06-19 21:55:43 -0300

    add the permission to access your location

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData.dark(),
+      theme: ThemeData.light(),
+      // theme: ThemeData.dark(), --> dark mode
+
       home: LoadingScreen(),
     );
   }

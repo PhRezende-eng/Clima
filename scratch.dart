466cbe642ba6a12d2eaf28dbc7e52434d1b6cfc3
PhRezende-eng <ph.hr.001@gmail.com>
2021-06-20 20:31:03 -0300

    learn to use future and async await functions

---
+++ scratch.dart
@@ -0,0 +1,29 @@
+void main() {
+  performTasks();
+}
+
+void performTasks() async {
+  task1();
+  String taskData2 = await task2();
+  task3(taskData2);
+}
+
+void task1() {
+  String result = 'task1 data';
+  print('Task 1 complete');
+}
+
+Future task2() async {
+  Duration duration = Duration(seconds: 3);
+  String result;
+  await Future.delayed(duration, () {
+    result = 'Task 2 complete';
+    print('Task 2 ...');
+  });
+  return result;
+}
+
+void task3(String taskData2) {
+  String result = 'task3 data';
+  print('Task 3 complete with $taskData2');
+}
